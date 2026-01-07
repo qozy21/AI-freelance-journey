3eb9b35b0ab91dbe78abea997b6b40e8220aacc1
qozy21 <qoz2in@gmail.com>
2026-01-08 06:48:13 +0700

    Add basic arithmetic functions to math_basic.py

--- python/math_package/modul-math_basic.py
+++ python/math_package/modul-math_basic.py
@@ -1,1 +1,19 @@
+def add(a, b):
+  """Menambahkan dua angka"""
+  return a + b
+
+def subtract(a, b):
+  """Mengurangi dua angka"""
+  return a - b
+
+def multiply(a, b):
+  """Mengalikan dua angka"""
+  return a * b
+
+def divide(a, b): 
+  """Membagi dua angka"""
+  if b == 0:
+    raise ValueError("tidak dapat membagi dengan angka nol!") 
+  return a / b
+
 
